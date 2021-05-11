90c2290dd48cdd041ff823f9457cf1f4debbf79d
Diego González <diegoztrombone@gmail.com>
2021-05-11 21:30:30 +0200

    feat: modificaciones de molpe, topo aparece y desaparece, añadido addeventlistener pero sin quitar topo, creado objeto de topos

--- app.js
+++ app.js
@@ -2,55 +2,78 @@ let count = 0
 const mole1 = document.querySelector("#topo1")
 const counter = document.querySelector("#counter")
 
-let moleInterval = setInterval(
-    () => {
-        moleVisibility(mole1)
-    }, 2000
-)
-
-function moleInit (node) {
-    node.style.visibility="visible"
-    moleEvent(node)                 
+const mole = {
+    node: mole1,
+    visible: false,
+    id: -1,
 }
 
-function moleVisibility (node) {
-    
-    if (node.style.visibility === "visible") {
+function getRandom() {
+    return Math.floor(Math.random() * 2000)
+}
+
+function init(mole) {
+    mole.id = setInterval(
+        () => {
+            console.info('> id: ', mole.id)
+            moleVisibility(mole)
+        }, getRandom()
+    )
+    setEvent(mole)
+}
+
+
+// function moleInit (node) {
+//     node.style.visibility="visible"
+//     moleEvent(node)                 
+// }
+
+function moleVisibility (mole) {
+    if(mole.visible) {
         console.log ("adios topo")
-        return node.style.visibility = "hidden"
-    }
-    if (node.style.visibility === "hidden") {
+        mole.node.style.visibility = 'hidden'
+        mole.visible = !mole.visible // false
+    } else {
         console.log("hola topo")
-        return node.style.visibility = "visible"
+        mole.node.style.visibility = 'visible'
+        mole.visible = !mole.visible // true
     }        
 }
-function moleEvent (node) {
-    node.addEventListener("click", (event) => {
-        if (event) {
-            node.style.visibility="hidden"
-            console.log("topo a la luna")
-            count++
-            counter.innerHTML=`<p>Número de topos enviados a la luna: ${count}</p>` 
-            stopInterval()   
-            setTimeout(() => {
-                node.style.visibility="visible"
-                console.log("aparece topo")
-                if (node.style.visibility === "visible") {
-                    console.log("vuelve a empezar el intervalo")
-                    moleInterval = setInterval(() => {
-                        moleVisibility(mole1)    
-                    }, 2000) 
-                      
-                }}, 5000)
-        }             
+
+function setEvent(mole) {
+    mole.node.addEventListener('click', function() {
+        console.info('> Hola!')
     })
-    
 }
 
-function stopInterval () {
-    console.log("parar")
-    clearInterval(moleInterval)
-    
-}
+init(mole)
 
-moleInit(mole1)
+// function moleEvent (node) {
+//     node.addEventListener("click", (event) => {
+//         if (event) {
+//             node.style.visibility="hidden"
+//             console.log("topo a la luna")
+//             count++
+//             counter.innerHTML=`<p>Número de topos enviados a la luna: ${count}</p>` 
+//             stopInterval(moleInterval)   
+//             setTimeout(() => {
+//                 node.style.visibility="visible"
+//                 console.log("aparece topo")
+//                 if (node.style.visibility === "visible") {
+//                     console.log("vuelve a empezar el intervalo")
+//                     moleInterval = setInterval(() => {
+//                         moleVisibility(mole1)    
+//                     }, 2000) 
+                      
+//                 }}, 5000)
+//         }             
+//     })
+    
+// }
+
+// function stopInterval (id) {
+//     console.log("parar")
+//     clearInterval(id)
+// }
+
+// moleInit(mole1)
